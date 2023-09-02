75ea33d000f097a298b8046357fc4cda757cf9c9
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-02 18:57:25 +0200

    LD suppressing the errors for now

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,7 +5,7 @@ import './navbar.css';
 // import { loggedInAtom } from '../../atoms/loggedInAtom';
 import Logo from './logo';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookBookmark, faUser, faEnvelope, faHome, faSignOut, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+// import { faBookBookmark, faUser, faEnvelope, faHome, faSignOut, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   // const loggedIn = useAtomValue(loggedInAtom);
@@ -24,76 +24,76 @@ const Navbar = () => {
             {isMobile ? (
               <>
                 <Link to="/books" className="menuLink">
-                  <FontAwesomeIcon icon={faBookBookmark} className="icon" title="Books" />
+                  {/* <FontAwesomeIcon icon={faBookBookmark} className="icon" title="Books" /> */}
                   <span>Books</span>
                 </Link>
                 <Link to="/about" className="menuLink">
-                  <FontAwesomeIcon icon={faEnvelope} className="icon" title="Contact" />
+                  {/* <FontAwesomeIcon icon={faEnvelope} className="icon" title="Contact" /> */}
                   <span>Notre projet</span>
                 </Link>
-                {loggedIn ? (
-                  <>
+                {/* {loggedIn ? (
+                  <> */}
                     <Link to="/profile" className="menuLink">
-                      <FontAwesomeIcon icon={faUser} className="icon" title="Profile" />
+                      {/* <FontAwesomeIcon icon={faUser} className="icon" title="Profile" /> */}
                       <span>Profile</span>
                     </Link>
                     {/* <Link to="/about" className="menuLink">
                       <FontAwesomeIcon icon={faEnvelope} className="icon" title="Contact" />
                       <span>Contact</span>
                     </Link> */}
-                    <div className="logoutWrapper">
+                    {/* <div className="logoutWrapper">
                       <Logout />
-                    </div>
-                  </>
+                    </div> */}
+                  {/* </>
                 ) : (
-                  <>
+                  <> */}
                     <Link to="/users/sign_in" className="menuLink">
-                      <FontAwesomeIcon icon={faSignInAlt} className="icon" title="Sign In" />
+                      {/* <FontAwesomeIcon icon={faSignInAlt} className="icon" title="Sign In" /> */}
                       <span>Sign In</span>
                     </Link>
                     <Link to="/users" className="menuLink">
-                      <FontAwesomeIcon icon={faUser} className="icon" title="Sign Up" />
+                      {/* <FontAwesomeIcon icon={faUser} className="icon" title="Sign Up" /> */}
                       <span>S'inscrire</span>
                     </Link>
-                  </>
-                )}
+                  {/* </>
+                )} */}
               </>
             ) : (
               <>
                 <Link to="/books" className="menuLink" title="Books">
-                  <FontAwesomeIcon icon={faBookBookmark} className="icon" />
+                  {/* <FontAwesomeIcon icon={faBookBookmark} className="icon" /> */}
                   <span>Books</span>
                 </Link>
                 <Link to="/about" className="menuLink">
-                  <FontAwesomeIcon icon={faEnvelope} className="icon" title="Contact" />
+                  {/* <FontAwesomeIcon icon={faEnvelope} className="icon" title="Contact" /> */}
                   <span>Notre projet</span>
                 </Link>
-                {loggedIn ? (
-                  <>
+                {/* {loggedIn ? (
+                  <> */}
                     <Link to="/profile" className="menuLink" title="Profile">
-                      <FontAwesomeIcon icon={faUser} className="icon" />
+                      {/* <FontAwesomeIcon icon={faUser} className="icon" /> */}
                       <span>Profile</span>
                     </Link>
                     {/* <Link to="/about" className="menuLink" title="Contact">
                       <FontAwesomeIcon icon={faEnvelope} className="icon" />
                       <span>Contact</span>
                     </Link> */}
-                    <div className="logoutWrapper">
+                    {/* <div className="logoutWrapper">
                       <Logout />
-                    </div>
-                  </>
+                    </div> */}
+                  {/* </>
                 ) : (
-                  <>
+                  <> */}
                     <Link to="/users/sign_in" className="menuLink" title="Sign In">
-                      <FontAwesomeIcon icon={faSignInAlt} className="icon" />
+                      {/* <FontAwesomeIcon icon={faSignInAlt} className="icon" /> */}
                       <span>Sign In</span>
                     </Link>
                     <Link to="/users" className="menuLink" title="Sign Up">
-                      <FontAwesomeIcon icon={faUser} className="icon" />
+                      {/* <FontAwesomeIcon icon={faUser} className="icon" /> */}
                       <span>S'inscrire</span>
                     </Link>
-                  </>
-                )}
+                  {/* </>
+                )} */}
               </>
             )}
           </div>
